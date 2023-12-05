1b768b836a5f13c72a8d40a1c3bc06b5c86c05c5
parkjg20 <parkjg20@naver.com>
2023-12-05 13:12:13 +0900

    [chapter 3] 3층 신경망 구현 연습 및 실습

---
+++ Chapter3/3_4_3_TripleLayeredNeuralNetworkExample.py
@@ -0,0 +1,33 @@
+import numpy as np
+from GraphSigmoidFunction import sigmoid
+
+## 3.4. 3층 신경망 구현 예제 코드
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+# 순전파(출력을 순방향으로 전달)를 나타내기 위한 네이밍, 역전파(backward)도 추후 학습
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = a3 # identity_function 생략
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
